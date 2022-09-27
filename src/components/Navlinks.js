c8524e26426cf024adb7a7a76091c182daac701f
Quincy Smith-Assan <87392433+quincy-S@users.noreply.github.com>
2022-09-27 17:00:25 +0000

    Update Navlinks.js

--- src/components/Navlinks.js
+++ src/components/Navlinks.js
@@ -4,11 +4,11 @@ import React from "react"
 const Navlinks = (props) => {
     return(
         <ul className={props.show ?"changeDisplay nav-links":"nav-links"}>
-//             <li>Collections</li>
-//             <li>Men</li>
-//             <li>Women</li>
-//             <li>About</li>
-//             <li>Contact</li>
+            <li>ctions</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
         </ul>
     )
 }
